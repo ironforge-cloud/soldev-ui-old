a5c7dc313b71dcf197de51f202bc37a479107af3
Italo A. Casas <me@italoacasas.com>
2021-10-08 15:48:46 +0200

    twitter timelime improvements

--- components/mini-social.js
+++ components/mini-social.js
@@ -1,34 +1,25 @@
-import React, { useEffect } from "react";
+import { Timeline } from "react-twitter-widgets";
+import React from "react";
 
 function MiniSocial() {
-  useEffect(() => {
-    const anchor = document.createElement("a");
-    anchor.setAttribute("class", "twitter-timeline");
-    anchor.setAttribute(
-      "data-chrome",
-      "noheader nofooter noborders transparent"
-    );
-    anchor.setAttribute("data-dnt", "true");
-    anchor.setAttribute(
-      "href",
-      "https://twitter.com/soldevapp/lists/1444990678371651585?ref_src=twsrc%5Etfw"
-    );
-    document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
-
-    const script = document.createElement("script");
-    script.setAttribute("src", "https://platform.twitter.com/widgets.js");
-    document.getElementsByClassName("twitter-embed")[0].appendChild(script);
-  }, []);
-
   return (
-    <aside className="hidden w-96 bg-white rounded-lg shadow-lg border overflow-hidden 2xl:block">
+    <aside className="hidden w-96 bg-white rounded-lg shadow-lg border overflow-hidden xl:block">
       <div className="mt-6 px-4">
         <h2 className="text-gray-500 text-xs font-medium uppercase tracking-wide mb-3">
           Social Timeline
         </h2>
-        <section className="twitterContainer">
-          <div className="twitter-embed"></div>
-        </section>
+
+        <Timeline
+          dataSource={{
+            sourceType: "list",
+            ownerScreenName: "soldevapp",
+            id: "1444990678371651585",
+          }}
+          options={{
+            dnt: true,
+            chrome: "noheader nofooter noborders transparent",
+          }}
+        />
       </div>
     </aside>
   );
