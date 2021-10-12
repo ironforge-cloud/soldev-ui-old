1b8e5ca3b6d645fa0613b95617d1108cc6c45218
Italo A. Casas <me@italoacasas.com>
2021-10-12 13:36:08 +0200

    small fix

--- components/gm.js
+++ components/gm.js
@@ -6,7 +6,8 @@ export default function TweetGm() {
       <a
         className="flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-blue-500 hover:bg-blue-400"
         href="https://twitter.com/intent/tweet?text=gm"
-        rel="noopener"
+        rel="noreferrer"
+        target="_blank"
       >
         <FontAwesomeIcon icon={["fab", "twitter"]} size="1x" color="white" />
         <span className="ml-1">tweet gm</span>
