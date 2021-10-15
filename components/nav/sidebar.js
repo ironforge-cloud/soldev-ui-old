0a5b64a306e7a6f46c099750ad5c56292aac7b34
Italo A. Casas <me@italoacasas.com>
2021-10-15 14:38:27 +0200

    progress

--- components/nav/sidebar.js
+++ components/nav/sidebar.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 const others = [
   { name: "Submit", href: "/submit", disabled: false },
   { name: "Newsletter", href: "/newsletter", disabled: false },
-  { name: "Roadmap", href: "/#", disabled: true },
+  { name: "Blog", href: "/#", disabled: true },
   { name: "FAQ", href: "/faq", disabled: false },
 ];
 
