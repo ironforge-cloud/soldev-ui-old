9c9ef88e097135ebf85859532346efdbdfa638c1
Italo A. Casas <me@italoacasas.com>
2021-11-18 01:06:20 +0100

    adding ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "tailwindcss/tailwind.css";
 
+
 // FontAwesome
 import { config, library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -12,6 +13,7 @@ library.add(fab, fas);
 import PlausibleProvider from "next-plausible";
 import Nav from "../components/nav";
 import dynamic from "next/dynamic";
+import Script from "next/script"
 
 // Import Swiper styles
 import "swiper/css";
@@ -56,7 +58,23 @@ export default function App({ Component, pageProps }) {
   const size = videoID ? "2xl" : "xl";
 
   return (
-    <>
+    <div>
+      <Script
+          id="1"
+          src="https://www.googletagmanager.com/gtag/js?id=G-HT8DFYDG03"
+      />
+      <Script
+          id="2"
+          dangerouslySetInnerHTML={{
+            __html: `
+          if (window.location.hostname === "localhost") window['ga-disable-G-HT8DFYDG03'] = true;
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HT8DFYDG03');
+          `,
+          }}
+      />
       <PlausibleProvider domain="soldev.app" trackOutboundLinks={true}>
         <WalletConnectionProvider>
           <WalletModalProvider logo="/logo-white.png">
