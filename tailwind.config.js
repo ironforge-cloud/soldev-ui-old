63173fc9ea1762b0a5316746458083489a78f4b1
Italo A. Casas <me@italoacasas.com>
2021-12-09 20:49:12 +0100

    updating tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -13,39 +12,10 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
-
       screens: {
         "3xl": "1700px",
       },
-      width: {
-        "video-3xl": "1030px",
-        "video-2xl": "865px",
-        "video-xl": "1030px",
-        "video-lg": "780px",
-        "video-md": "700px",
-        "video-sm": "570px",
-        "video-xs": "426px",
-        card: "360px",
-      },
-      height: {
-        "video-3xl": "579px",
-        "video-2xl": "492px",
-        "video-xl": "579px",
-        "video-lg": "439px",
-        "video-md": "396px",
-        "video-sm": "321px",
-        "video-xs": "240px",
-      },
-      colors: {
-        green: colors.emerald,
-        purple: colors.violet,
-        yellow: colors.amber,
-      },
     },
-  },
-
-  variants: {
-    extend: {},
   },
   plugins: [
     require("@tailwindcss/forms"),
