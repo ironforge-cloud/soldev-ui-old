59f5ba1bff3217767ff26cb1aa0a339ce8387bfd
Italo A. Casas <me@italoacasas.com>
2021-12-09 20:57:52 +0100

    video player improvements

--- components/videos/player.js
+++ components/videos/player.js
@@ -3,39 +3,35 @@ import PropTypes from "prop-types";
 
 export default function Player({ content }) {
   return (
-    <div className="grid grid-cols-6 pt-16 pb-10 justify-items-center">
-      <div className="flex flex-col col-span-6 items-center w-video-xs sm:w-video-sm md:w-video-md lg:w-video-lg xl:w-video-xl 2xl:w-video-2xl 3xl:w-video-3xl xs:text-xs text-base">
-        <div className="self-end mb-1">
-          <a
-            href={content.Url}
-            className="text-purple-600 hover:text-purple-700"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p className="text-lg pb-1 hover:underline">
-              Watch in {content.Provider} &rarr;{" "}
-            </p>
-          </a>
-        </div>
-        <div
-          className="w-video-xs h-video-xs sm:w-video-sm sm:h-video-sm 
-      md:w-video-md md:h-video-md lg:w-video-lg lg:h-video-lg xl:w-video-xl xl:h-video-xl 2xl:w-video-2xl 2xl:h-video-2xl 3xl:w-video-3xl 3xl:h-video-3xl"
+    <div className="flex flex-col max-w-screen-2xl mx-auto p-16 xs:text-xs text-base">
+      <div className="self-end mb-1">
+        <a
+          href={content.Url}
+          className="text-purple-600 hover:text-purple-700"
+          target="_blank"
+          rel="noreferrer"
         >
-          <ReactPlayer
-            width="100%"
-            height="100%"
-            url={content.Url}
-            controls
-            pip
-            stopOnUnmount={false}
-          />
-        </div>
-        <div className="self-start md:text-lg">
-          <h1 className=" font-medium text-gray-800 sm:mt-4 xl:text-xl 3xl:text-2xl">
-            {content.Title}
-          </h1>
-          <p className="mt-2 text-gray-600 sm:mt-4">{content.Description}</p>
-        </div>
+          <p className="text-lg pb-1 hover:underline">
+            Watch in {content.Provider} &rarr;{" "}
+          </p>
+        </a>
+      </div>
+
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        className="aspect-[16/9] w-full"
+        url={content.Url}
+        controls
+        pip
+        stopOnUnmount={false}
+      />
+
+      <div className="self-start md:text-lg">
+        <h1 className=" font-medium text-gray-800 sm:mt-4 xl:text-xl 3xl:text-2xl">
+          {content.Title}
+        </h1>
+        <p className="mt-2 text-gray-600 sm:mt-4">{content.Description}</p>
       </div>
     </div>
   );
