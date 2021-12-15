cfee97e50b33eec303e89f816596ed7cbf17b501
Italo A. Casas <me@italoacasas.com>
2021-12-15 10:58:29 +0100

    adding infinity loading to twitter component

--- components/dashboard/sidebar.js
+++ components/dashboard/sidebar.js
@@ -1,6 +1,6 @@
 import useTweets from "../../hooks/useTweets";
 import Tweet from "../twitter/tweet";
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import NetworkStatus from "./network-status";
 import usePinnedTweets from "../../hooks/usePinnedTweets";
 
@@ -8,6 +8,38 @@ function Sidebar() {
   const { data: tweets = [], tweetsLoading } = useTweets();
   const { data: pinnedTweets = [], isLoading: pinnedTweetsLoading } =
     usePinnedTweets();
+  const [loadMore, setLoadMore] = useState(false);
+
+  // This helper function allow me to have infinity loading without having
+  // to build pagination in the api
+  function loadTweets(tweets) {
+    let size = 0;
+    // If there are enough tweets we load 10, if not we load array.length
+    if (Array.isArray(tweets) && tweets.length > 0) {
+      tweets.length >= 10 ? (size = 10) : (size = tweets.length);
+    }
+
+    // if loadMore then we want to render all the tweets
+    if (loadMore) size = tweets.length;
+
+    let component = [];
+    for (let i = 0; i < size; i++) {
+      component.push(
+        <Tweet
+          key={tweets[i].id}
+          text={tweets[i].text}
+          author={tweets[i].Author}
+          id={tweets[i].id}
+          media={tweets[i].Media}
+          created_at={tweets[i].created_at}
+          public_metrics={tweets[i].public_metrics}
+          referenced_tweets={tweets[i].ReferenceTweets}
+        />
+      );
+    }
+
+    return component;
+  }
 
   return (
     <div className="flex flex-warp flex flex-col 3xl:flex-row gap-6 ">
@@ -60,29 +92,19 @@ function Sidebar() {
           </h2>
           <div className="mt-6 flow-root">
             <div role="list" className="-my-4 divide-y divide-gray-200">
-              {Array.isArray(tweets) &&
-                tweets.map((tweet) => (
-                  <Tweet
-                    key={tweet.id}
-                    text={tweet.text}
-                    author={tweet.Author}
-                    id={tweet.id}
-                    media={tweet.Media}
-                    created_at={tweet.created_at}
-                    public_metrics={tweet.public_metrics}
-                    referenced_tweets={tweet.ReferenceTweets}
-                  />
-                ))}
+              {Array.isArray(tweets) && loadTweets(tweets)}
             </div>
           </div>
-          <div className="mt-6">
-            <a
-              href="#"
-              className="w-full block text-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-            >
-              View all
-            </a>
-          </div>
+          {!loadMore && (
+            <div className="mt-6">
+              <button
+                onClick={() => setLoadMore(true)}
+                className="w-full block text-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+              >
+                View all
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
