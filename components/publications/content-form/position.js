514dc980a9f387976f27fec92dcd4a5e774957fc
Italo A. Casas <me@italoacasas.com>
2021-12-30 19:12:42 +0100

    small css fix

--- components/publications/content-form/position.js
+++ components/publications/content-form/position.js
@@ -30,13 +30,14 @@ function Position({ data, setData, list }) {
       <select
         id="position"
         name="position"
-        className="mt-1  max-w-fit block w-full pl-3 pr-10 py-2 text-base border-gray-300 dark:bg-stone-400 dark:border-stone-500 dark:text-stone-800 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+        className="mt-1  block w-auto pl-3 pr-10 py-2 text-base border-gray-300 dark:bg-stone-400 dark:border-stone-500 dark:text-stone-800 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
         value={position}
         onChange={(e) =>
           setData({ ...data, Position: parseInt(e.target.value) })
         }
       >
         {list.map((item) => {
+          if (item === 0) return <option key={item}>Remove Sort</option>;
           return <option key={item}>{item}</option>;
         })}
       </select>
