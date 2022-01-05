8e1fb9916602b336783c5363f4e7de84bc1da5f1
Sayantan Karmakar <sayantankarmakar@outlook.com>
2022-01-05 21:03:11 +0530

    data separation

---
+++ pages/directory/data.temp.js
@@ -0,0 +1,295 @@
+export const directory = [
+  {
+    Name: "Protocols",
+    Projects: [
+      {
+        Title: "Mango Markets",
+        ID: "mango",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        Title: "Raydium",
+        ID: "raydium",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        Title: "Serum",
+        ID: "serum",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        Title: "Quarry Protocols",
+        ID: "quarry",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+  },
+  {
+    Name: "RPC Providers",
+    Projects: [
+      {
+        Title: "Triton",
+        ID: "triton",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        Title: "GenesysGO",
+        ID: "genesys",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+  },
+  {
+    Name: "Oracles",
+    Projects: [
+      {
+        Title: "Pyth Network",
+        ID: "pyth",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        Title: "Chainlink",
+        ID: "chainlink",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+  },
+  {
+    Name: "Program Library",
+    Projects: [
+      {
+        Title: "Name Service",
+        ID: "sns",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+      {
+        Title: "Token Lending",
+        ID: "lending",
+        Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      },
+    ],
+  },
+];
+
+export const projects = {
+  mango: {
+    Name: "Mango Markets",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "mango",
+    About: "long text",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  triton: {
+    Name: "Triton",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "triton",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  serum: {
+    Name: "Serum",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "serum",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  raydium: {
+    Name: "Raydium",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "raydium",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  quarry: {
+    Name: "Quarry Protocol",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "quarry",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  genesys: {
+    Name: "GenesysGO",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "genesys",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  pyth: {
+    Name: "Pyth",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "pyth",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  chainlink: {
+    Name: "Chainlink",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "chainlink",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  lending: {
+    Name: "Token Lending",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "lending",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  lending: {
+    Name: "Token Lending",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "lending",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+  sns: {
+    Name: "Solana Name Service",
+    Logo: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    ID: "sns",
+    About: "a very very long description",
+    Links: [
+      {
+        type: "Site",
+        href: "https://url.com",
+      },
+      {
+        type: "Discord",
+        href: "https://url.com",
+      },
+      {
+        type: "Documentation",
+        href: "https://url.com",
+      },
+    ],
+  },
+};
