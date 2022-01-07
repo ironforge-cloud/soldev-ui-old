cf5c3051fd975102b3175023c54ef3e7b8da8702
Italo A. Casas <me@italoacasas.com>
2022-01-07 19:29:40 +0100

    adding cli

--- utils/tags.js
+++ utils/tags.js
@@ -1,6 +1,15 @@
 export default {
   level: ["Beginner", "Intermediate", "Expert"],
   tech: ["Anchor", "Arweave", "Metaplex", "Web3.js", "Serum", "Solana"],
-  language: ["Rust", "JavaScript", "C#", "Golang", "Python", "Java", "Swift"],
+  language: [
+    "Rust",
+    "JavaScript",
+    "C#",
+    "Golang",
+    "Python",
+    "Java",
+    "Swift",
+    "CLI",
+  ],
   badge: ["New", "Hot", "Best", "Old"],
 };
