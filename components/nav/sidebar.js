d98670208fde9d987ca411b567f9814c4594105f
Italo A. Casas <me@italoacasas.com>
2022-01-09 19:02:08 +0100

    adding missing min-w-full to navigation

--- components/nav/sidebar.js
+++ components/nav/sidebar.js
@@ -110,7 +110,7 @@ function NavSidebar({ closeMobileMenu }) {
                   item.name === current
                     ? "bg-gray-200 text-gray-900 dark:bg-stone-800 dark:text-stone-200"
                     : "text-gray-600 dark:text-stone-200",
-                  "group flex items-center px-3 py-2 text-lg lg:text-sm font-medium rounded-md max-w-[190px] min-w-[150px] cursor-pointer",
+                  "min-w-full group flex items-center px-3 py-2 text-lg lg:text-sm font-medium rounded-md max-w-[190px] min-w-[150px] cursor-pointer",
                   item.disabled
                     ? "opacity-50"
                     : "hover:bg-gray-50 dark:hover:bg-stone-700"
@@ -137,7 +137,7 @@ function NavSidebar({ closeMobileMenu }) {
                   item.name === current
                     ? "bg-gray-200 text-gray-900 dark:bg-stone-800 dark:text-stone-200"
                     : "text-gray-800 dark:text-stone-300",
-                  "group flex items-center px-3 py-2 text-lg lg:text-sm font-medium rounded-md max-w-[190px] min-w-[150px]",
+                  "min-w-full group flex items-center px-3 py-2 text-lg lg:text-sm font-medium rounded-md max-w-[190px] min-w-[150px]",
                   item.disabled
                     ? "opacity-20"
                     : "hover:bg-gray-50 dark:hover:bg-stone-700"
