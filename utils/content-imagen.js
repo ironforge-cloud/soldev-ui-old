4bb06adcab40d9b88ccaa437b383beb152d76cdc
Italo A. Casas <me@italoacasas.com>
2022-02-05 19:37:01 +0100

    fixing twitter space img placeholder

---
+++ utils/content-imagen.js
@@ -0,0 +1,17 @@
+/**
+ * Card Image helper function
+ * @param {Object} content
+ * @return {String}
+ */
+export default function defineImage(content) {
+    if (content.Img) {
+        return content.Img
+    } else if (content.ContentType === "threads") {
+        return "/twitter-placeholder.webp"
+    } else if (content.Url.includes("twitter") && content.ContentType === "podcasts") {
+        return "/twitter-placeholder.webp"
+    }
+
+
+    return "/placeholder.webp"
+}
