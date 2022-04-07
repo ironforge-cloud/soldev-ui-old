1b7b8fca66ad1e7ad74bd28446c974b7caa44501
Italo Casas <me@italoacasas.com>
2022-04-07 08:36:27 -0400

    progress

--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,13 @@
-import Head from "next/head";
-import Nav from "./nav";
-import PropTypes from "prop-types";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 // Wallet Auth
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import PropTypes from "prop-types";
 // App Context
 import { AppWrapper } from "../context/AppContext";
-import dynamic from "next/dynamic";
+import Nav from "./nav";
 
 const WalletConnectionProvider = dynamic(
   () => import("../components/wallet-connection-provider"),
@@ -16,45 +16,45 @@ const WalletConnectionProvider = dynamic(
   }
 );
 
-export function Container({ children, metaTags }) {
+export function Container({children, metaTags}) {
   return (
     <div>
       <Head>
         <title>{metaTags.title}</title>
-        <meta name="title" content={metaTags.title} />
-        <meta name="description" content={metaTags.description} />
+        <meta name="title" content={metaTags.title}/>
+        <meta name="description" content={metaTags.description}/>
 
         {/* Google */}
         {metaTags.shouldIndex ? (
           <>
             {" "}
-            <meta name="robots" content="index,follow,noodp" />
-            <meta name="googlebot" content="index,follow" />
+            <meta name="robots" content="index,follow,noodp"/>
+            <meta name="googlebot" content="index,follow"/>
           </>
         ) : (
           <>
-            <meta name="robots" content="noindex" />
-            <meta name="googlebot" content="noindex" />
+            <meta name="robots" content="noindex"/>
+            <meta name="googlebot" content="noindex"/>
           </>
         )}
 
         {/* Open Graph */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={metaTags.url} />
-        <meta property="og:title" content={metaTags.title} />
-        <meta property="og:description" content={metaTags.description} />
-        <meta property="og:image" content="https://soldev.app/banner.png" />
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content={metaTags.url}/>
+        <meta property="og:title" content={metaTags.title}/>
+        <meta property="og:description" content={metaTags.description}/>
+        <meta property="og:image" content="https://soldev.app/banner.png"/>
 
         {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@soldevapp" />
-        <meta name="twitter:creator" content="@italoacasas" />
-        <meta name="twitter:url" content={metaTags.url} />
-        <meta name="twitter:title" content={metaTags.title} />
-        <meta name="twitter:description" content={metaTags.description} />
-        <meta name="twitter:image" content="https://soldev.app/banner.png" />
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:site" content="@soldevapp"/>
+        <meta name="twitter:creator" content="@italoacasas"/>
+        <meta name="twitter:url" content={metaTags.url}/>
+        <meta name="twitter:title" content={metaTags.title}/>
+        <meta name="twitter:description" content={metaTags.description}/>
+        <meta name="twitter:image" content="https://soldev.app/banner.png"/>
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <WalletConnectionProvider>
--- components/wallet-connection-provider.js
+++ components/wallet-connection-provider.js
@@ -1,27 +1,26 @@
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { ConnectionProvider, WalletProvider, } from "@solana/wallet-adapter-react";
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from "@solana/web3.js";
+import { clusterApiUrl } from '@solana/web3.js';
 import { memo, useMemo } from "react";
 
 function WalletConnectionProvider({children}) {
-    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
-    const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
-    // You can also provide a custom RPC endpoint
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  // You can also provide a custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
-    // Only the wallets you configure here will be compiled into your application
-    const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking --
+  // Only the wallets you configure here will be compiled into your application
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
-    return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
-                {children}
-            </WalletProvider>
-        </ConnectionProvider>
-    );
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        {children}
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
 export default memo(WalletConnectionProvider);
