09a6ab011b3cc7f3dc89a69a623cb57264ed166f
Italo Casas <me@italoacasas.com>
2022-04-07 07:58:31 -0400

    adding banner

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,5 +1,6 @@
-import { memo, useState } from "react";
 import dynamic from "next/dynamic";
+import { memo, useState } from "react";
+import Banner from "./banner";
 import TopBar from "./topbar";
 
 const NavSidebar = dynamic(() => import("./nav-sidebar"));
@@ -8,19 +9,20 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-function Nav({ children }) {
+function Nav({children}) {
   const [search, setSearch] = useState(false);
 
   return (
     <div className={classNames(search && "min-h-[5050px] xl:min-h-[3500px]")}>
       <div className="w-full z-50">
-        <TopBar setSearch={setSearch} />
+        <TopBar setSearch={setSearch}/>
+        <Banner/>
       </div>
 
       <div className="min-h-full">
         <div className="flex sm:pl-6 lg:pl-8 lg:gap-8 py-7">
           <div className="min-w-[190px] hidden lg:block content-between">
-            <NavSidebar />
+            <NavSidebar/>
           </div>
 
           <div className="w-full overflow-hidden">{!search && children}</div>
