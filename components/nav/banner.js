59a64db7848af3c24a3033185d0602e22a58e8b6
Italo Casas <me@italoacasas.com>
2022-04-07 10:44:18 -0400

    small fix

--- components/nav/banner.js
+++ components/nav/banner.js
@@ -7,7 +7,7 @@ function Banner() {
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="pr-16 sm:text-center sm:px-16">
           <p className="font-medium text-white">
-            <span className="md:hidden">We announced a new product!</span>
+            <span className="md:hidden">New Jobs board powered by Superteam Crypto Jobs.</span>
             <span
               className="hidden md:inline">Big news! New Jobs board powered by Superteam Crypto Jobs.</span>
             <span className="block sm:ml-2 sm:inline-block">
