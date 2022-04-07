20ea9b70b24d3d9039fce9d69827fd643435b5f0
Italo Casas <me@italoacasas.com>
2022-04-07 08:02:25 -0400

    removing banner close btn

--- components/nav/banner.js
+++ components/nav/banner.js
@@ -1,4 +1,3 @@
-import { XIcon } from '@heroicons/react/outline'
 import Link from "next/link"
 import { memo } from "react"
 
@@ -22,16 +21,7 @@ function Banner() {
             </span>
           </p>
         </div>
-        <div
-          className="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
-          <button
-            type="button"
-            className="flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white"
-          >
-            <span className="sr-only">Dismiss</span>
-            <XIcon className="h-6 w-6 text-white" aria-hidden="true"/>
-          </button>
-        </div>
+        
       </div>
     </div>
   )
