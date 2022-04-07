0ec21ccc841ca368042f77e7bcde5e1ebffc77a0
Italo Casas <me@italoacasas.com>
2022-04-07 05:19:34 -0400

    updating dependencies

--- next.config.js
+++ next.config.js
@@ -2,68 +2,70 @@ const {withSentryConfig} = require("@sentry/nextjs");
 const {withPlausibleProxy} = require("next-plausible");
 
 const withTM = require("next-transpile-modules")([
-    "@solana/wallet-adapter-base",
-    "@solana/wallet-adapter-react",
-    "@solana/wallet-adapter-wallets",
-    "@solana/wallet-adapter-react-ui",
-    "@solana/wallet-adapter-phantom",
+  "@solana/wallet-adapter-base",
+  "@solana/wallet-adapter-react",
+  "@solana/wallet-adapter-wallets",
+  "@solana/wallet-adapter-react-ui",
+  "@solana/wallet-adapter-phantom",
 ]);
 
 const moduleExports = withPlausibleProxy()(
-    withTM({
-        experimental: {},
-        swcMinify: true,
-        reactStrictMode: true,
-        images: {
-            domains: [
-                "i.ytimg.com",
-                "images.unsplash.com",
-                "solana.com",
-                "static-cdn.jtvnw.net",
-                "clips-media-assets2.twitch.tv",
-                "www.youtube.com",
-                "pbs.twimg.com",
-                "opengraph.githubassets.com",
-                "res.cloudinary.com",
-                "repository-images.githubusercontent.com",
-                "figment.io",
-                "lorisleiva.com",
-                "www.notion.so",
-                "dev.to",
-                "twitter.com",
-                "alexgr.in",
-                "avatars.githubusercontent.com",
-                "solanacookbook.com",
-                "solana.ghost.io",
-                "www.gravatar.com",
-                "api.typedream.com",
-                "blog.neodyme.io",
-                "lh4.googleusercontent.com",
-                "www.jpmti2016.com",
-            ],
-            formats: ["image/avif", "image/webp"],
+  withTM({
+    experimental: {
+      runtime: 'nodejs',
+    },
+    swcMinify: true,
+    reactStrictMode: true,
+    images: {
+      domains: [
+        "i.ytimg.com",
+        "images.unsplash.com",
+        "solana.com",
+        "static-cdn.jtvnw.net",
+        "clips-media-assets2.twitch.tv",
+        "www.youtube.com",
+        "pbs.twimg.com",
+        "opengraph.githubassets.com",
+        "res.cloudinary.com",
+        "repository-images.githubusercontent.com",
+        "figment.io",
+        "lorisleiva.com",
+        "www.notion.so",
+        "dev.to",
+        "twitter.com",
+        "alexgr.in",
+        "avatars.githubusercontent.com",
+        "solanacookbook.com",
+        "solana.ghost.io",
+        "www.gravatar.com",
+        "api.typedream.com",
+        "blog.neodyme.io",
+        "lh4.googleusercontent.com",
+        "www.jpmti2016.com",
+      ],
+      formats: ["image/avif", "image/webp"],
+    },
+    async rewrites() {
+      return [
+        {
+          source: "/",
+          destination: "/library",
         },
-        async rewrites() {
-            return [
-                {
-                    source: "/",
-                    destination: "/library",
-                },
-            ];
-        },
-    })
+      ];
+    },
+  })
 );
 
 const SentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
