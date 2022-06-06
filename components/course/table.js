934a86839a60b15dc8f900e8868a544a82d08fd9
Italo Casas <me@italoacasas.com>
2022-06-06 15:40:07 -0400

    fixing id issues

--- components/course/table.js
+++ components/course/table.js
@@ -4,14 +4,13 @@ import TableRow from './table-row';
 import { modules } from '../../utils/course-map';
 
 function Table() {
-  const id = useId();
   return (
     <div className="mx-auto my-20 flex max-w-4xl flex-col gap-10">
       <div>
         <TableHeader ready title="Client interaction with the Solana Network" subTitle="Module 1" />
 
         {modules[0].map((item, index) => {
-          return <TableRow ready item={item} index={index} key={id} />;
+          return <TableRow ready item={item} index={index} key={index} />;
         })}
       </div>
 
@@ -23,7 +22,7 @@ function Table() {
         />
 
         {modules[1].map((item, index) => {
-          return <TableRow ready item={item} index={index} key={id} />;
+          return <TableRow ready item={item} index={index} key={index} />;
         })}
       </div>
 
@@ -31,7 +30,7 @@ function Table() {
         <TableHeader title="Basic Solana Program Development" subTitle="Module 3" />
 
         {modules[2].map((item, index) => {
-          return <TableRow item={item} index={index} key={id} />;
+          return <TableRow item={item} index={index} key={index} />;
         })}
       </div>
 
@@ -39,7 +38,7 @@ function Table() {
         <TableHeader title="Advanced Solana Program Development" subTitle="Module 4" />
 
         {modules[3].map((item, index) => {
-          return <TableRow item={item} index={index} key={id} />;
+          return <TableRow item={item} index={index} key={index} />;
         })}
       </div>
     </div>
