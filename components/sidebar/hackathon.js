f2a2876a7886f3d8320215cd98303320834ce300
Italo Casas <me@italoacasas.com>
2022-08-09 11:28:08 +0200

    small fix

--- components/sidebar/hackathon.js
+++ components/sidebar/hackathon.js
@@ -7,14 +7,8 @@ function Hackathon() {
   return (
     <section className="rounded-lg p-6 shadow ">
       <div className="mb-5 flex flex-1 gap-2">
-        <span className="flex rounded-lg p-2 text-gray-900 dark:text-gray-200">
-          <Image
-            src="/ironforge.png"
-            alt="Ironforge"
-            width="25"
-            height="25"
-            className="h-10 w-11"
-          />
+        <span className="flex rounded-lg pt-2 pr-1 text-gray-900 dark:text-gray-200">
+          <Image src="/ironforge.png" alt="Ironforge" width="25" height="25" className="h-8 w-9" />
           {/* <SpeakerphoneIcon className="h-6 w-6" aria-hidden="true" /> */}
         </span>
         <p className="prose flex flex-col font-medium ">
@@ -24,12 +18,12 @@ function Hackathon() {
           </span>
         </p>
       </div>
-      <Link href="/course">
+      <a href="https://www.ironforge.cloud/?utm_source=soldev.app" target="_blank" rel="noreferrer">
         <a className="text-md flex items-center justify-center gap-2 rounded-md border border-none bg-gradient-to-r from-pink-500 to-blue-500 px-4 py-2 font-medium text-gray-50 shadow-sm hover:shadow-xl">
           <ExternalLinkIcon className="h-5 w-5" aria-hidden="true" />
           Visit
         </a>
-      </Link>
+      </a>
     </section>
   );
 }
