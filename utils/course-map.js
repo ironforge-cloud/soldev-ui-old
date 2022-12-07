f27b49506500758ddf4ac69cb1da1e2d21ff9634
James Pacheco <jamesrp13@gmail.com>
2022-12-07 11:25:39 -0700

    feat: add initial security lessons

--- utils/course-map.js
+++ utils/course-map.js
@@ -92,5 +92,77 @@ export const modules = [
       title: 'Anchor CPIs and errors',
       link: '/course/anchor-cpi'
     }
+  ],
+  [
+    {
+      title: 'Lesson 1',
+      link: ''
+    },
+    {
+      title: 'Lesson 2',
+      link: ''
+    },
+    {
+      title: 'Lesson 3',
+      link: ''
+    },
+    {
+      title: 'Lesson 4',
+      link: ''
+    }
+  ],
+  [
+    {
+      title: 'How to approach the Program Security module',
+      link: '/course/security-intro'
+    },
+    {
+      title: 'How to approach the Program Security module',
+      link: ''
+    },
+    {
+      title: 'Signer authorization',
+      link: '/course/signer-auth'
+    },
+    {
+      title: 'Owner checks',
+      link: '/course/owner-checks.md'
+    },
+    {
+      title: 'Account data matching',
+      link: '/course/account-data-matching.md'
+    },
+    {
+      title: 'Reinitialization attacks - COMING SOON',
+      link: ''
+    },
+    {
+      title: 'Duplicate mutable accounts - COMING SOON',
+      link: ''
+    },
+    {
+      title: 'PDA sharing - COMING SOON',
+      link: ''
+    },
+    {
+      title: 'Type cosplay - COMING SOON',
+      link: ''
+    },
+    {
+      title: 'Bump seed canonicalization - COMING SOON',
+      link: ''
+    },
+    {
+      title: 'Arbitrary CPIs - COMING SOON',
+      link: ''
+    },
+    {
+      title: 'Closing accounts - COMING SOON',
+      link: ''
+    },
+    {
+      title: 'Sysvar address checking - COMING SOON',
+      link: ''
+    }
   ]
 ];
