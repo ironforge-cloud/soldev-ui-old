b061abb1acf905074310932333ae8ccdea1315e5
nickfrosty <nfrostbutter@gmail.com>
2022-12-07 23:18:34 -0500

    fix: fixed the link display

--- components/course/table-row.js
+++ components/course/table-row.js
@@ -11,16 +11,14 @@ function TableRow({ item, index, ready }) {
     <div
       className={classNames(
         'dark:bg-gray- flex items-center justify-between space-x-2 border-x border-b border-gray-300  py-3 px-3 dark:border-gray-600',
-        ready
-          ? 'cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700'
-          : 'cursor-not-allowed blur-sm'
+        ready ? ' hover:bg-gray-200 dark:hover:bg-gray-700' : 'cursor-not-allowed blur-sm'
       )}
     >
-      <Link href={item.link} passHref>
-        <div className="w-full">
+      <Link href={item.link}>
+        <a className="">
           <span className="mr-3 text-xl text-gray-400 dark:text-gray-500">{++index}.</span>
           <span className=" tracking-wide text-gray-700 dark:text-gray-300">{item.title}</span>
-        </div>
+        </a>
       </Link>
 
       <CheckMark item={item.link} />
