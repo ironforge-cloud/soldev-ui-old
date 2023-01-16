89394a618874588e29fb6cbf85036bfcf93bd1ac
Italo Casas <me@italo.dev>
2023-01-15 22:56:07 -0500

    small fix

--- utils/get-base-url.js
+++
@@ -1,6 +0,0 @@
-export const getBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') return 'https://soldev.app/';
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview')
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-  return process.env.NEXT_PUBLIC_SITE_URL;
-};
