e4a89a8f9aec8e49e14eda440a5d42bd13483cb4
Italo Casas <me@italo.dev>
2023-01-15 22:30:19 -0500

    small fix

--- pages/registry/index.js
+++ pages/registry/index.js
@@ -98,13 +98,13 @@ export default function Registry({ data }) {
                 </div>
               </div>
               <div>
-                <div className="-mt-px flex divide-x divide-gray-200 dark:divide-gray-500">
+                <div className="-mt-px flex divide-x divide-gray-200 dark:divide-gray-600">
                   <div className="flex w-0 flex-1">
                     <button
                       onClick={() => downloadIDL(program)}
                       className="relative -mr-px inline-flex w-0 flex-1 items-center
                       justify-center rounded-bl-lg border border-transparent py-4 text-sm
-                      font-medium text-gray-700 hover:text-gray-500 dark:border-gray-500 dark:text-gray-300 hover:dark:text-gray-100"
+                      font-medium text-gray-700 hover:text-gray-500 dark:border-gray-600 dark:text-gray-300 hover:dark:text-gray-100"
                     >
                       <DownloadIcon className="h-5 w-5 text-gray-300 " aria-hidden="true" />
                       <span className="ml-3">Download</span>
@@ -115,7 +115,7 @@ export default function Registry({ data }) {
                       href={`/registry/${program.address}`}
                       className="relative inline-flex w-0 flex-1 items-center justify-center
                        rounded-br-lg border border-transparent py-4 text-sm font-medium
-                        text-gray-700 hover:text-gray-500 dark:border-gray-500 dark:text-gray-300 hover:dark:text-gray-100"
+                        text-gray-700 hover:text-gray-500 dark:border-gray-600 dark:text-gray-300 hover:dark:text-gray-100"
                     >
                       <ClipboardListIcon className="h-5 w-5 text-gray-300" aria-hidden="true" />
                       <span className="ml-3">Details</span>
