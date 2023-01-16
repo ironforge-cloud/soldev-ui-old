5726db7c242f4483b1eafd735885243ae9fd6e5a
Italo Casas <me@italo.dev>
2023-01-15 22:15:39 -0500

    small fix

--- pages/registry/index.js
+++ pages/registry/index.js
@@ -104,9 +104,9 @@ export default function Registry({ data }) {
                       onClick={() => downloadIDL(program)}
                       className="relative -mr-px inline-flex w-0 flex-1 items-center
                       justify-center rounded-bl-lg border border-transparent py-4 text-sm
-                      font-medium text-gray-700 hover:text-gray-500 dark:border-gray-500 dark:text-gray-300"
+                      font-medium text-gray-700 hover:text-gray-500 dark:border-gray-500 dark:text-gray-300 hover:dark:text-gray-100"
                     >
-                      <DownloadIcon className="h-5 w-5 text-gray-300" aria-hidden="true" />
+                      <DownloadIcon className="h-5 w-5 text-gray-300 " aria-hidden="true" />
                       <span className="ml-3">Download</span>
                     </button>
                   </div>
@@ -115,7 +115,7 @@ export default function Registry({ data }) {
                       href={`/registry/${program.address}`}
                       className="relative inline-flex w-0 flex-1 items-center justify-center
                        rounded-br-lg border border-transparent py-4 text-sm font-medium
-                        text-gray-700 hover:text-gray-500 dark:border-gray-500 dark:text-gray-300"
+                        text-gray-700 hover:text-gray-500 dark:border-gray-500 dark:text-gray-300 hover:dark:text-gray-100"
                     >
                       <ClipboardListIcon className="h-5 w-5 text-gray-300" aria-hidden="true" />
                       <span className="ml-3">Details</span>
